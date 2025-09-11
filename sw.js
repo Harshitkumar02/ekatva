13025f4876d47505362cb2975c20f810d03ab06f
Harshit Kumar <harshitsinghhs878@gmail.com>
2025-09-11 17:27:44 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,36 @@
+const STATIC_CACHE = 'static-v1';
+const ASSETS = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== STATIC_CACHE).map((k) => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) return cached;
+      return fetch(request).then((response) => {
+        const copy = response.clone();
+        caches.open(STATIC_CACHE).then((cache) => cache.put(request, copy));
+        return response;
+      }).catch(() => caches.match('/index.html'));
+    })
+  );
+});
+
 // Service Worker for Ekatva Welfare Foundation
 const CACHE_NAME = 'ekatva-v1';
 const urlsToCache = [
